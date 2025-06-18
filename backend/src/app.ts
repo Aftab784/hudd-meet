22da5628203cf85c04b13ca9bee0e6d8481da926
Aftab784 <af227656@gmail.com>
2025-06-18 23:07:00 +0500

    DB Model

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,8 @@ const PORT = process.env.PORT as string || 3000
 
 app.set("port", PORT)
 app.use(cors())
-app.use(express())
+app.use(express.json({limit: '40kb'}))
+app.use(express.urlencoded({limit: '40kb', extended : true}))
 
 
 
