4e949177f46f600b3b9af37a7b11e5159aadc1c6
Aftab784 <af227656@gmail.com>
2025-06-23 16:24:07 +0500

    Socket Code Structure

--- backend/src/controllers/SocketManager.ts
+++ backend/src/controllers/SocketManager.ts
@@ -1,10 +1,60 @@
-import { Server } from "socket.io"
-import { Server as HttpServer } from "node:http"
+import { Server } from "socket.io";
+import { Server as HttpServer } from "node:http";
+
+
+let connections = {};
+let messages = {};
+let timeOnline = {};
 
 const connectToSocket = (server: HttpServer) => {
-    const io = new Server(server)
+  const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ['Get', 'Post']
+    }
+  });
+
+  io.on("connection", (socket) => {
+    console.log("New client connected: ", socket.id)
+
+    socket.on("join-call", (roomId:string, username: string) => {
+       
+    });
+
+    socket.on("signal", (toId, message) => {
+
+    });
+
+    socket.on("chat-message", (data, sender) => {
+
+    });
     
-    return io;
-}
+    socket.on("reaction", ()=> {
+        
+    })
+    
+    socket.on("raise-hand", ()=> {
+
+    })
+
+    socket.on("mute-all", ()=> {
+
+    })
+    
+    socket.on("kick-user", ()=> {
+        
+    })
+    
+    socket.on("disconnecting", ()=> {
+        
+    })
+    
+    socket.on("disconnect", () => {
+    
+    });
+
+  });
+  return io;
+};
 
 export default connectToSocket;
